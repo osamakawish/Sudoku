2fe394547dfbdef5783dd2ac75982db22460d4b0
Osama Kawish <osamakawish@gmail.com>
2017-10-14 11:49:14 -0400

    Added basic useful methods for Sudoku class, to be defined.

--- Sudoku/board.cpp
+++ Sudoku/board.cpp
@@ -1,12 +1,34 @@
 #include "stdafx.h"
 #include "board.h"
+#include <vector>
+#include <random>
+using namespace std;
 
 
-board::board(int size, int difficulty)
+board::board(int gSize, int diff) : gameSize(gSize), difficulty(diff)
 {
+	
 }
-
 
 board::~board()
 {
+}
+
+vector<vector<int>> board::createRandomGame()
+{
+	return vector<vector<int>>();
+}
+
+void board::hasSolution()
+{
+}
+
+int board::row(int, int)
+{
+	return 0;
+}
+
+int board::col(int, int)
+{
+	return 0;
 }
--- Sudoku/board.h
+++ Sudoku/board.h
@@ -2,7 +2,14 @@
 class board
 {
 public:
-	board(int size, int diffifuclty);
+	board(int gameSize, int diffifuclty);
 	~board();
-};
 
+private:
+	int gameSize; int difficulty;
+	vector<vector<int>> game;
+	vector<vector<int>> createRandomGame(); // Creates a random workable board.
+	void hasSolution(); // Checks if there's a solution from the current state.
+	int row(int, int); // Determines the row of cell given block index and cell index inside block.
+	int col(int, int); // Determines the col of cell given block index and cell index inside block.
+};
